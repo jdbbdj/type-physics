c8bff7b9d40534e848d8e4527e01080f2b90dfcd
jamesdarylbertiz@gmail.com <75722677+jdbbdj@users.noreply.github.com>
2023-03-31 18:53:18 +0800

    Initialize: Starter

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import { Canvas } from "@react-three/fiber";
+
+function App() {
+  return <Canvas children={undefined}></Canvas>;
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+const container = document.getElementById("root") as HTMLElement;
+if (container) {
+  const root = createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  console.warn("No element with ID root found");
+}
